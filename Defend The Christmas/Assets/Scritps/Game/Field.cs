7bebf0f0a11666892edbdf0a34cad8952d27681e
Nikita Ivanov <nikishka_95@mail.ru>
2015-10-31 12:40:21 +0500

    Version 2
    - изменён размер карты на 14x7
    - дорожка теперь бесшовная

--- Defend The Christmas/Assets/Scritps/Game/Field.cs
+++ Defend The Christmas/Assets/Scritps/Game/Field.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Field : MonoBehaviour {
 	//STRUCTS
@@ -16,11 +17,16 @@ public class Field : MonoBehaviour {
 	public Vector2 StartPosition;
 	public Instruction[] MoveInstructions;
 	public GameObject PathObject;
+	public Texture2D path_horizontal;
+	public Texture2D[] path_corners;
 	//STATIC VARIABLES
 	static Vector2 Step;
+	static int MapWidth = 14;
+	static int MapHeight = 7;
 	// Use this for initialization
 	void Start () {
-		Step = new Vector2 (0.1f, 0.2f);
+		Step = new Vector2 ((float)1 / MapWidth, (float)1 / MapHeight);
+		StartPosition = new Vector2 (StartPosition.x, MapHeight - StartPosition.y);
 		PlacePath ();
 	}
 	
@@ -53,6 +59,24 @@ public class Field : MonoBehaviour {
 					break;
 				}
 				GameObject pObj = Instantiate(PathObject);
+				if ((j==MoveInstructions[i].Steps-1) && (i<MoveInstructions.Length-1))
+				{
+					int k=1;
+					if (((MoveInstructions[i].Direction==MoveDirection.Down) && (MoveInstructions[i+1].Direction==MoveDirection.Right)) || ((MoveInstructions[i].Direction==MoveDirection.Left) && (MoveInstructions[i+1].Direction==MoveDirection.Up)))
+						k=0;
+					if (((MoveInstructions[i].Direction==MoveDirection.Up) && (MoveInstructions[i+1].Direction==MoveDirection.Right)) || ((MoveInstructions[i].Direction==MoveDirection.Left) && (MoveInstructions[i+1].Direction==MoveDirection.Down)))
+						k=1;
+					if (((MoveInstructions[i].Direction==MoveDirection.Right) && (MoveInstructions[i+1].Direction==MoveDirection.Down)) || ((MoveInstructions[i].Direction==MoveDirection.Up) && (MoveInstructions[i+1].Direction==MoveDirection.Left)))
+						k=2;
+					if (((MoveInstructions[i].Direction==MoveDirection.Right) && (MoveInstructions[i+1].Direction==MoveDirection.Up)) || ((MoveInstructions[i].Direction==MoveDirection.Down) && (MoveInstructions[i+1].Direction==MoveDirection.Left)))
+						k=3;
+					pObj.GetComponent<Image>().sprite = Sprite.Create(path_corners[k], new Rect(0,0,path_horizontal.width, path_horizontal.height), new Vector2(0,0));
+				}
+				else
+				{
+					if ((MoveInstructions[i].Direction==MoveDirection.Right) || (MoveInstructions[i].Direction==MoveDirection.Left))
+						pObj.GetComponent<Image>().sprite = Sprite.Create(path_horizontal, new Rect(0,0,path_horizontal.width, path_horizontal.height), new Vector2(0,0));
+				}
 				pObj.GetComponent<RectTransform>().SetParent(this.transform, false);
 				pObj.GetComponent<RectTransform>().anchorMin = currentPosition;
 				pObj.GetComponent<RectTransform>().anchorMax = currentPosition + Step;
